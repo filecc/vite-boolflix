5c49e7753cbbfa7ed5e6069d802f8ef064445f5d
filecc <fil.gia@live.it>
2023-04-19 14:09:17 +0200

    catching 404 not found route

--- router/index.js
+++ router/index.js
@@ -2,12 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: () => import('../views/Home.vue')},
+  { path: '/404', component: () => import('../views/NotFound.vue') },  
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('../views/NotFound.vue') },  
   { path: '/search',component: () => import('../views/Search.vue')},
   { path: '/movie/:name', component: () => import('../views/MovieGeneric.vue') },
   { path: '/series/:name', component: () => import('../views/TvGeneric.vue') },
-]
+];
+
+
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.resolve({
+    name: 'NotFound',
+    params: { pathMatch: ['not', 'found'] },
+  }).href // '/not/found';
+
 export default router
---
+++ views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Nothing to see here
+        <router-link to="/">Go Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
